73928325e96fc0f38c98f2a5dbaddc019f379b72
keeran.paheerathan <keeran.paheerathan@gmail.com>
2021-04-30 11:45:27 +0200

    Changed saving to .gif to saving to .avi

--- generate_image_semi.py
+++ generate_image_semi.py
@@ -1,5 +1,7 @@
 import time
 import sys
+import cv2
+import numpy
 from math_utils import draw_triangle
 from pathlib import Path
 from PIL import Image
@@ -14,7 +16,7 @@ processed_image_directory = str(real_image_name.parent)
 real_image_pixels = real_image.load()
 final_image = Image.new('RGB', real_image.size, 'white')
 successful_iteration = 0
-gif_images = [final_image]
+images = [final_image]
 
 i = 1
 successful_iteration = 0
@@ -26,21 +28,29 @@ while successful_iteration/i > 0.1 or i < 100:
         final_image = new_image
         successful_iteration += 1
         if(successful_iteration % 100 == 0):
-            gif_images.append(final_image.copy())
+            images.append(final_image.copy())
     if i % 2000 == 0:
         print('Successful Iteration: ' + str(successful_iteration) + ', iteration: ' + str(i))
     SUCCESSFUL_ITERATION.append(successful_iteration)
     i += 1
-
-final_image.save(processed_image_directory+ '/' + real_image_name.stem + '_processed.jpg')
-gif_images[0].save(
-    processed_image_directory+ '/' + real_image_name.stem + '_processed.gif',
-    save_all=True,
-    append_images=gif_images[1:],
-    optimize=True,
-    loop=0
-)
 real_image.close()
-# os.system('ffmpeg -i out/out.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" video.mp4')
+
+''' Saving processed image '''
+
+### Save processed image as .jpg ###
+final_image.save(processed_image_directory+ '/' + real_image_name.stem + '_processed.jpg')
+
+### Save the processing as .avi ###
+out = cv2.VideoWriter(
+    processed_image_directory+ '/' + real_image_name.stem + '_processed.avi',
+    cv2.VideoWriter_fourcc(*'DIVX'),
+    10,
+    final_image.size
+)
+
+for img in images:
+    opencv_image = numpy.array(img.convert('RGB'))[:, :, ::-1] #Converting PIL image into OpenCV image
+    out.write(opencv_image)
+out.release()
+
 print("--- %s seconds ---" % (time.time() - start_time))
-final_image.show()
