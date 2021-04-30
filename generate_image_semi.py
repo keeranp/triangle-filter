e1e21a5bf2101ce79aeb6e4d29bdc94e4ae2ca27
keeran.paheerathan <keeran.paheerathan@gmail.com>
2021-04-30 09:35:08 +0200

    Initial commit

---
+++ generate_image_semi.py
@@ -0,0 +1,108 @@
+import time
+import math
+import operator
+from functools import reduce
+import os
+import numpy as np
+from PIL import Image, ImageDraw, ImageChops
+from random import randint
+from matplotlib import pyplot as plt
+
+def random_position(w, h):
+    return (randint(0, w), randint(0, h))
+
+def get_real_image_color(points, real_imagep, size):
+    center_x = int((points[0][0] + points[1][0] + points[2][0])/3)
+    center_y = int((points[0][1] + points[1][1] + points[2][1])/3)
+    color = list(real_imagep[min(size[0]-1,center_x),min(center_y,size[1]-1)])
+    color[0] = color[0] + randint(-50,50)
+    color[1] = color[1] + randint(-50,50)
+    color[2] = color[2] + randint(-50,50)
+    return tuple(color)
+
+def draw_triangle(image, real_image, real_image_pixels):
+    first_point = random_position(image.size[0], image.size[1])
+    second_point = (first_point[0] + randint(-70, 70),
+                    first_point[1] + randint(-70, 70))
+    third_point = (second_point[0] + randint(-70, 70),
+                   second_point[1] + randint(-70, 70))
+    coords = [first_point, second_point, third_point]
+    color = get_real_image_color(coords, real_image_pixels, image.size)
+
+    crop_points = (min(first_point[0],second_point[0],third_point[0]), min(first_point[1],second_point[1],third_point[1]),
+    max(first_point[0],second_point[0],third_point[0],image.size[0]), max(first_point[1],second_point[1],third_point[1],image.size[1]))
+    cropped_image = image.crop(crop_points)
+    cropped_real_image = real_image.crop(crop_points)
+
+    previous_rmse = rmse(cropped_real_image, cropped_image)
+
+    draw = ImageDraw.Draw(image)
+    draw.polygon(coords,color)
+
+    cropped_image = image.crop(crop_points)
+    cropped_real_image = real_image.crop(crop_points)
+
+    rmserr = rmse(cropped_real_image, cropped_image)
+    return rmserr < previous_rmse
+
+def rmse(img1, img2):
+    h = ImageChops.difference(img1, img2).histogram()
+    rmserr = math.sqrt(reduce(operator.add,map(lambda h, i: h*(i**2), h, range(256))) / (float(img1.size[0]) * img1.size[1]))
+    return rmserr
+
+start_time = time.time()
+
+SUCCESSFUL_ITERATION = []
+
+real_image = Image.open('img\\landscape.jpg')
+real_image_pixels = real_image.load()
+final_image = Image.new('RGB', real_image.size, 'white')
+successful_iteration = 0
+gif_images = [final_image]
+gif_images[0].save(
+    'out\\out.gif',
+    save_all=True,
+    append_images=gif_images[1:],
+    duration=40,
+    optimize=True,
+    loop=0
+)
+
+i = 1
+successful_iteration = 0
+final_image.save('out\\out_0.jpg')
+# for i in range(MAX_ITERATION):
+while successful_iteration/i > 0.1 or i<100:
+    new_image = final_image.copy()
+    if draw_triangle(new_image, real_image, real_image_pixels):
+        final_image = new_image
+        successful_iteration += 1
+        if(successful_iteration % 100 == 0):
+            gif_images.append(final_image.copy())
+            gif_images[0].save(
+                'out\\out.gif',
+                save_all=True,
+                append_images=gif_images[1:],
+                optimize=True,
+                loop=0
+            )
+    if i % 2000 == 0:
+        final_image.save('out\\out_'+str(i)+'.jpg')
+        print('Successful Iteration: ' + str(successful_iteration) + ', iteration: ' + str(i))
+    SUCCESSFUL_ITERATION.append(successful_iteration)
+    i+=1
+    
+final_image.save('out\\out_final.jpg')
+gif_images[0].save(
+    'out\\out.gif',
+    save_all=True,
+    append_images=gif_images[1:],
+    optimize=True,
+    loop=0
+)
+real_image.close()
+os.system('ffmpeg -i out/out.gif -movflags faststart -pix_fmt yuv420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" video.mp4')
+print("--- %s seconds ---" % (time.time() - start_time))
+final_image.show()
+plt.plot(np.arange(0,i-1,1),SUCCESSFUL_ITERATION)
+plt.show()
