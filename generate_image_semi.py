2435c6bffa19109e9aa4c21f86c7dab4ebc7c220
keeran.paheerathan <keeran.paheerathan@gmail.com>
2021-04-30 15:46:35 +0200

    f

--- generate_image_semi.py
+++ generate_image_semi.py
@@ -2,47 +2,55 @@ import time
 import sys
 import cv2
 import numpy
-from math_utils import draw_triangle
+from math_utils import process_image
 from pathlib import Path
 from PIL import Image
 
+MAX_ITERATION = 50000
+
 start_time = time.time()
 
-SUCCESSFUL_ITERATION = []
-
+''' Getting the filename and the directory to save the processed files '''
 real_image_name = Path(sys.argv[1])
-real_image = Image.open(sys.argv[1])
-processed_image_directory = str(real_image_name.parent)
-real_image_pixels = real_image.load()
-final_image = Image.new('RGB', real_image.size, 'white')
-successful_iteration = 0
-images = [final_image]
+image_directory = str(real_image_name.parent)
 
+''' Setting up the processed image '''
+#real_image = Image.open(real_image_name)
+real_image = cv2.imread(str(real_image_name))
+#real_image_pixels = real_image.load()
+final_image = Image.new('RGB', (1920, 1080), 'white')
+image_size_ratio = (real_image.shape[0]/final_image.size[0],real_image.shape[0]/final_image.size[0])
+images = [final_image] #Array containing all the images for the video
+
+''' Processing the image '''
 i = 1
 successful_iteration = 0
-final_image.save(processed_image_directory+ '/' + real_image_name.stem + '_processed.jpg')
+continue_processing = True
 
-while successful_iteration/i > 0.1 or i < 100:
+while continue_processing:
     new_image = final_image.copy()
-    if draw_triangle(new_image, real_image, real_image_pixels):
+    if process_image(new_image, real_image, image_size_ratio, len(sys.argv) >= 3):
         final_image = new_image
         successful_iteration += 1
         if(successful_iteration % 100 == 0):
             images.append(final_image.copy())
     if i % 2000 == 0:
-        print('Successful Iteration: ' + str(successful_iteration) + ', iteration: ' + str(i))
-    SUCCESSFUL_ITERATION.append(successful_iteration)
+        print('Successful Iteration: ' + str(successful_iteration) + ', iteration: ' + str(i) + ', ' + str(successful_iteration/i))
+    if(len(sys.argv) >= 3):
+        continue_processing = successful_iteration/i > 0.1 or i < 100
+    else:
+        continue_processing = i < MAX_ITERATION
     i += 1
+
 real_image.close()
 
 ''' Saving processed image '''
-
 ### Save processed image as .jpg ###
-final_image.save(processed_image_directory+ '/' + real_image_name.stem + '_processed.jpg')
+final_image.save(image_directory+ '/' + real_image_name.stem + '_processed.jpg')
 
 ### Save the processing as .avi ###
 out = cv2.VideoWriter(
-    processed_image_directory+ '/' + real_image_name.stem + '_processed.avi',
+    image_directory+ '/' + real_image_name.stem + '_processed.avi',
     cv2.VideoWriter_fourcc(*'DIVX'),
     10,
     final_image.size
--- math_utils.py
+++ math_utils.py
@@ -7,43 +7,63 @@ from functools import reduce
 def random_position(w, h):
     return (randint(0, w), randint(0, h))
 
-
 def get_real_image_color(points, real_imagep, size):
+    ''' Gets the color of the pixel at the center of a triangle '''
     center_x = int((points[0][0] + points[1][0] + points[2][0])/3)
     center_y = int((points[0][1] + points[1][1] + points[2][1])/3)
-    color = list(real_imagep[min(size[0]-1, center_x),
-                             min(center_y, size[1]-1)])
+    color = list(real_imagep[min(size[0]-1, center_x), min(center_y, size[1]-1),2])
     color[0] = color[0] + randint(-50, 50)
     color[1] = color[1] + randint(-50, 50)
     color[2] = color[2] + randint(-50, 50)
     return tuple(color)
 
+def create_triangle_coords(image_size):
+    ''' Create randomly coordinates of a triangle on an image according to the image size '''
+    first_point = random_position(image_size[0], image_size[1])
+    second_point = (first_point[0] + randint(-50, 50),
+                    first_point[1] + randint(-50, 50))
+    third_point = (second_point[0] + randint(-50, 50),
+                   second_point[1] + randint(-50, 50))
+    return [first_point, second_point, third_point]
 
-def draw_triangle(image, real_image, real_image_pixels):
-    first_point = random_position(image.size[0], image.size[1])
-    second_point = (first_point[0] + randint(-70, 70),
-                    first_point[1] + randint(-70, 70))
-    third_point = (second_point[0] + randint(-70, 70),
-                   second_point[1] + randint(-70, 70))
-    coords = [first_point, second_point, third_point]
-    color = get_real_image_color(coords, real_image_pixels, image.size)
-
-    crop_points = (min(first_point[0], second_point[0], third_point[0]), min(first_point[1], second_point[1], third_point[1]),
-                   max(first_point[0], second_point[0], third_point[0], image.size[0]), max(first_point[1], second_point[1], third_point[1], image.size[1]))
-    cropped_image = image.crop(crop_points)
-    cropped_real_image = real_image.crop(crop_points)
-
-    previous_rmse = rmse(cropped_real_image, cropped_image)
-
+def draw_triangle(image, coords, color):
     draw = ImageDraw.Draw(image)
     draw.polygon(coords, color)
 
-    cropped_image = image.crop(crop_points)
-    cropped_real_image = real_image.crop(crop_points)
+def process_image(image, real_image, image_size_ratio, precise):
+    coords = create_triangle_coords(real_image.shape)
+    color = get_real_image_color(coords, real_image, real_image.size)
 
-    rmserr = rmse(cropped_real_image, cropped_image)
-    return rmserr < previous_rmse
+    if precise:
+        crop_points = (
+            min(coords[0][0], coords[1][0], coords[2][0]), min(coords[0][1], coords[1][1], coords[2][1]),
+            max(coords[0][0], coords[1][0], coords[2][0], image.size[0]), max(coords[0][1], coords[1][1], coords[2][1], image.size[1])
+        )
 
+        cropped_image = image.crop(crop_points)
+        cropped_real_image = real_image.crop(crop_points)
+
+        previous_rmse = rmse(cropped_real_image, cropped_image)
+
+        test_image = image.copy()
+        draw_triangle(test_image, coords, color)
+
+        cropped_image = test_image.crop(crop_points)
+
+        rmserr = rmse(cropped_real_image, cropped_image)
+
+        if(rmserr < previous_rmse):
+            resized_coords = []
+            for coord in coords:
+                resized_coords.append((int(coord[0] / image_size_ratio[0]), int(coord[1] / image_size_ratio[1])))
+            draw_triangle(image, resized_coords, color)
+        return rmserr < previous_rmse
+    else:
+        resized_coords = []
+        for coord in coords:
+            resized_coords.append((int(coord[0] / image_size_ratio[0]), int(coord[1] / image_size_ratio[1])))
+        draw_triangle(image, resized_coords, color)
+        return True
 
 def rmse(img1, img2):
     h = ImageChops.difference(img1, img2).histogram()
